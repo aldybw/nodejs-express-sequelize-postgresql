96b7aa7c7aedb672e44f5ff4b2cf90d29b334679
aldyckybw <bagusaldycky@gmail.com>
2022-11-29 07:33:29 +0700

    init express with postgres

---
+++ server.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8080",
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+// .sync({ force: true })
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Synced db");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the node application" });
+});
+
+require("./app/routes/tutorial.routes")(app);
+
+// set port
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(
+    `Server is running on port ${PORT}, access this on http://localhost:${PORT}`
+  );
+});
